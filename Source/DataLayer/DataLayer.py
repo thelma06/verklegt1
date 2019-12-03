5520bedebd9317f96ff1ddc63ce5e9a80e17e5b5
Thorsteinnag <42977744+Thorsteinnag@users.noreply.github.com>
2019-12-03 22:02:40 +0000

    les úr csv

--- Source/DataLayer/DataLayer.py
+++ Source/DataLayer/DataLayer.py
@@ -1,12 +1,12 @@
 #Just a test file
 import _csv
 
-with open('Destination.csv') as csv_file:
+with open('Destinations.csv') as csv_file:
     csv_reader = _csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
+            print(f'Dálkanöfnin eru {", ".join(row)}')
             line_count += 1
         else:
             print(f'\t{row[0]} er id fyrir {row[1]}')
