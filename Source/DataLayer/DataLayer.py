af69db5eb313398608f5cb682621eb7d32addcf5
thelmada <54916456+thelmada@users.noreply.github.com>
2019-12-03 22:20:34 +0000

    add to csv komið

--- Source/DataLayer/DataLayer.py
+++ Source/DataLayer/DataLayer.py
@@ -1,14 +1,30 @@
 #Just a test file
 import _csv
 
-with open('Destinations.csv') as csv_file:
-    csv_reader = _csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print(f'\t{row[0]} er id fyrir {row[1]}')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+
+def get_destinations():
+    with open('Destinations.csv') as csv_file:
+        csv_reader = _csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            else:
+                ids,place = row
+                print(f'\t id: {ids} er id fyrir: {place}')
+                line_count += 1
+        #print(f'Processed {line_count} lines.')
+
+def add_destinations(new_ids,new_place):
+    with open('Destinations.csv','a') as csv_file:
+        csv_reader = _csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        myCSVrow = new_ids+','+new_place
+        csv_file.write(myCSVrow)
+
+
+new_ids = str('NewID')
+new_place = str('NewPlace')
+add_destinations(new_ids,new_place)
+get_destinations()
