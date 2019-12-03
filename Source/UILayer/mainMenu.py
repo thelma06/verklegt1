127312c3b3898ec44156d37b075272de2b0aaef3
gunnsteinng <gunnsteinng07@ru.is>
2019-12-03 19:13:35 +0000

    Smá byrjun á main menu :)

--- Source/UILayer/mainMenu.py
+++ Source/UILayer/mainMenu.py
@@ -1,1 +1,22 @@
-#This is main menu
+
+
+class Main_menu:
+
+    #def __init__(self):
+
+    def main_menu(self):
+        action = ""
+        while(action != "q"):
+            print("*********************************************")
+            print("*                                           *")
+            print("*            WELCOME TO NaN-AIR!            *")
+            print("*                                           *")
+            print("*********************************************")
+            print("")
+            print("1: Create")
+            print("2: Get")
+            print("3: Update")
+            print("q: Quit")
+            print("")
+
+            action = input("Choose an option: ").lower()
--- Source/_main_.py
+++ Source/_main_.py
@@ -1,5 +1,8 @@
-# Nothing here yet
-#test2
+from UILayer.mainMenu import Main_menu
+
+def main():
+    ui = Main_menu()
+    ui.main_menu()
 
 if __name__ == '__main__':
-    pass
+    main()
